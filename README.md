4020f82d113aa17af1e969dc238138c31d9b8ff4
Murat Guney <muratguney1995@gmail.com>
2021-06-02 23:27:44 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+> # @reactivers/use-click-outside
+
+[![npm version](https://badge.fury.io/js/@reactivers%2Fuse-click-outside.svg)](//www.npmjs.com/package/@reactivers/use-click-outside)
+
+# Welcome
+
+## Introduction
+
+> ```@reactivers/use-click-outside``` 
+## Docs
+You can find detailed docs [here](https://hooks.reactivers.com/use-click-outside)
+
+## Getting Started
+
+### Installation
+
+```bash
+npm install --save @reactivers/use-click-outside
+
+yarn add @reactivers/use-click-outside
+```
---
+++ dist/index.cjs.js
@@ -0,0 +1,39 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var react = require('react');
+
+var useClickOutside = function (_a) {
+    var ref = _a.ref, callback = _a.callback, _b = _a.withState, withState = _b === void 0 ? false : _b, _c = _a.passive, passive = _c === void 0 ? true : _c;
+    var _d = react.useState(false), clickedState = _d[0], setClickedState = _d[1];
+    var clickedRef = react.useRef(false);
+    var updateSwitch = react.useCallback(function (newValue) {
+        if (withState) {
+            setClickedState(newValue);
+        }
+        else {
+            clickedRef.current = newValue;
+        }
+    }, [withState]);
+    var onClick = react.useCallback(function (event) {
+        if (ref.current) {
+            if (!ref.current.contains(event.target)) {
+                updateSwitch(true);
+                callback(event);
+            }
+            else {
+                updateSwitch(false);
+            }
+        }
+    }, [ref.current, callback, updateSwitch]);
+    react.useEffect(function () {
+        document.addEventListener("click", onClick, { passive: passive });
+        return function () {
+            document.removeEventListener("click", onClick);
+        };
+    }, [onClick, passive]);
+    return withState ? clickedState : clickedRef.current;
+};
+
+exports.useClickOutside = useClickOutside;
